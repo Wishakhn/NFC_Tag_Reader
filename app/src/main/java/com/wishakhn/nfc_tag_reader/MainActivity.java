496c79c1050c1a51acba7fee2e96c712cd0cb5f8
Gul Wishah Khan <wishakhn@gmail.com>
2020-01-21 11:12:24 +0500

    Initial commit

--- app/src/main/java/com/wishakhn/nfc_tag_reader/MainActivity.java
+++ app/src/main/java/com/wishakhn/nfc_tag_reader/MainActivity.java
@@ -3,6 +3,7 @@ package com.wishakhn.nfc_tag_reader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void NFCHandler(View view) {
+        switch (view.getId()){
+            case R.id.startbtn:
+                break;
+            case R.id.stopbtn:
+                break;
+        }
     }
 }
